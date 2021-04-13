78eb68e0cf52d8c914d500ec5fc85be7508fc372
xtrannguyen <tnguyenmw@gmail.com>
2021-04-13 01:03:46 -0400

    generate a webpage

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
+
+// console.log(profileDataArgs);
 
 // Notice the lack of parentheses around the `profileDataArr` parameter?
-const printProfileData = profileDataArr => {
+// const printProfileData = profileDataArr => {
     // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
+   // for (let i = 0; i < profileDataArr.length; i += 1) {
+     // console.log(profileDataArr[i]);
+    //}
   
-    console.log('================');
+    //console.log('================');
   
     // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-  };
+    //profileDataArr.forEach(profileItem => console.log(profileItem));
+  //};
+
+  // const generatePage = () => 'Name: Jane, Github: janehub';
+  // const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
+
+
+  // console.log(name, github);
+ // console.log(generatePage(name,github));
+ fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
