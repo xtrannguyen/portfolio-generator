1ea61e893eace1bf6c1b1a8ae55c5b4d7d78dbdb
xtrannguyen <tnguyenmw@gmail.com>
2021-04-14 17:36:42 -0400

    updated template

--- app.js
+++ app.js
@@ -2,14 +2,6 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generatePage = require('./src/page-template');
 
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
-
 const promptUser = () => {
     return inquirer.prompt([
       {
@@ -145,6 +137,12 @@ const promptUser = () => {
   promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./index.html', pageHTML, err => {
+         if (err) throw err;
+
+          console.log('Portfolio complete! Check out index.html to see the output!');
+        });
 });
   
